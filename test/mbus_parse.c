f1c6d0ca6dc048ba46d5c6b00a12a984cbee80ce
Ralf Uhlig <ralf@uhlig-online.de>
2016-10-21 20:33:47 +0000

    Modified test tools for InfluxDB Line Protocol

--- test/mbus_parse.c
+++ test/mbus_parse.c
@@ -19,11 +19,11 @@ main(int argc, char *argv[])
 {
     FILE *fp = NULL;
     size_t buff_len, len, i;
-    int normalized = 0, json = 0;
+    int normalized = 0, json = 0, influxdb = 0;
     unsigned char buf[1024];
     mbus_frame reply;
     mbus_frame_data frame_data;
-    char *xml_result = NULL, *json_result = NULL, *file = NULL;
+    char *resultstr = NULL, *file = NULL;
 
     for (i = 1; i < argc; i++)
     {
@@ -34,6 +34,12 @@ main(int argc, char *argv[])
         else if (strcmp(argv[i], "-j") == 0)
         {
             json = 1;
+            influxdb = 0;
+        }
+        else if (strcmp(argv[i], "-i") == 0)
+        {
+            influxdb = 1;
+            json = 0;
         }
         else
         {
@@ -43,9 +49,10 @@ main(int argc, char *argv[])
 
     if (file == NULL)
     {
-        fprintf(stderr, "usage: %s [-n] [-j] binary-file\n", argv[0]);
+        fprintf(stderr, "usage: %s [-n] [-j|-i] binary-file\n", argv[0]);
         fprintf(stderr, "    optional flag -n for normalized values\n");
-        fprintf(stderr, "    optional flag -j for result as json string instead of xml string\n");
+        fprintf(stderr, "    optional flag -j for result as json string\n");
+        fprintf(stderr, "    optional flag -i for result as InfluxDB Line Protocol string\n");
         return 1;
     }
 
@@ -72,29 +79,41 @@ main(int argc, char *argv[])
     mbus_frame_data_parse(&reply, &frame_data);
     mbus_frame_print(&reply);
 
-    if (json)
+    if (influxdb)
     {
-        json_result = normalized ? mbus_frame_data_json_normalized(&frame_data) : mbus_frame_data_json(&frame_data);
+        resultstr = normalized ? mbus_frame_data_influxdb_normalized(&frame_data) : mbus_frame_data_influxdb(&frame_data);
 
-        if (json_result == NULL)
+        if (resultstr == NULL)
+        {
+            fprintf(stderr, "Failed to generate InfluxDB Line Protocol representation of MBUS frame: %s\n", mbus_error_str());
+            return 1;
+        }
+        printf("%s", resultstr);
+        free(resultstr);
+    }
+    else if (json)
+    {
+        resultstr = normalized ? mbus_frame_data_json_normalized(&frame_data) : mbus_frame_data_json(&frame_data);
+
+        if (resultstr == NULL)
         {
             fprintf(stderr, "Failed to generate JSON representation of MBUS frame: %s\n", mbus_error_str());
             return 1;
         }
-        printf("%s", json_result);
-        free(json_result);
+        printf("%s", resultstr);
+        free(resultstr);
     }
     else
     {
-        xml_result = normalized ? mbus_frame_data_xml_normalized(&frame_data) : mbus_frame_data_xml(&frame_data);
+        resultstr = normalized ? mbus_frame_data_xml_normalized(&frame_data) : mbus_frame_data_xml(&frame_data);
 
-        if (xml_result == NULL)
+        if (resultstr == NULL)
         {
             fprintf(stderr, "Failed to generate XML representation of MBUS frame: %s\n", mbus_error_str());
             return 1;
         }
-        printf("%s", xml_result);
-        free(xml_result);
+        printf("%s", resultstr);
+        free(resultstr);
     }
 
     return 0;
